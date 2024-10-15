4c9a7aaf6259d0768d4bfa43f6f587b7215e6f6d
JhonnyGames <borozdinaes89@gmail.com>
2024-10-15 23:51:44 +0500

    Add project files.

---
+++ Module7_OOP/Program.cs
@@ -0,0 +1,51 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Address address = new Address("Moscow", "Lenina", 10);
+        HomeDelivery hd = new HomeDelivery(address, "Ivan Ivanich");
+
+        hd.DeliveryStatus();
+    }
+
+    public class Address
+    {
+        public string city { get; set; }
+        public string street { get; set; }
+        public int house { get; set; }
+        public Address(string city, string street, int house)
+        {
+            this.city = city;
+            this.street = street;
+            this.house = house;
+        }
+        public override string ToString()
+        {
+            return $"Город: {city} улица {street} {house}";
+        }
+    }
+
+    public abstract class Delivery
+    {
+        public Address address { get; set; }
+        public Delivery(Address address)
+        {
+            this.address = address;
+        }
+        public abstract void DeliveryStatus();
+    }
+
+    class HomeDelivery : Delivery
+    {
+        public string courierName { get; set; }
+        public HomeDelivery(Address address, string courierName) : base(address)
+        {
+            this.courierName = courierName;
+        }
+        public override void DeliveryStatus()
+        {
+            Console.WriteLine($"Доставка на дом, по адресу: {address.ToString()} курьером {courierName}");
+        }
+    }
+
+}
