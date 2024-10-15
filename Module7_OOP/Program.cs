45344c516363c449880deff3e12cf6f214316d47
JhonnyGames <borozdinaes89@gmail.com>
2024-10-16 00:00:22 +0500

    class PickPointDelivery

--- Module7_OOP/Program.cs
+++ Module7_OOP/Program.cs
@@ -3,9 +3,10 @@
     private static void Main(string[] args)
     {
         Address address = new Address("Moscow", "Lenina", 10);
-        HomeDelivery hd = new HomeDelivery(address, "Ivan Ivanich");
+        HomeDelivery homeDelivery = new HomeDelivery(address, "Ivan Ivanich");
+        homeDelivery.DeliveryStatus();
 
-        hd.DeliveryStatus();
+
     }
 
     public class Address
@@ -35,7 +36,7 @@
         public abstract void DeliveryStatus();
     }
 
-    class HomeDelivery : Delivery
+    public class HomeDelivery : Delivery
     {
         public string courierName { get; set; }
         public HomeDelivery(Address address, string courierName) : base(address)
@@ -47,5 +48,19 @@
             Console.WriteLine($"Доставка на дом, по адресу: {address.ToString()} курьером {courierName}");
         }
     }
+
+    public class PickPointDelivery : Delivery
+    {
+        public PickPointDelivery(Address address) : base(address)
+        {
+            base.address = address;
+        }
+        public override void DeliveryStatus()
+        {
+            Console.WriteLine($"Доставка в пункт выдачи, по адресу: {address.ToString()}");
+        }
+    }
+
+    
 
 }
